211a021f69ef1f6ba788af34223b1f29f24a2649
MikeM501 <marcos.castillo91@student.qcc.cuny.edu>
2020-11-28 18:52:46 -0500

    JSOMN driven resume added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { MyResumeComponent } from './my-resume/my-resume.component';
+import { ResumeEducationComponent } from './resume-education/resume-education.component';
+import { ResumeFooterComponent } from './resume-footer/resume-footer.component';
+import { ResumeProfileComponent } from './resume-profile/resume-profile.component';
+import { ResumeWorkExperienceComponent } from './resume-work-experience/resume-work-experience.component';
 
 @NgModule({
   imports: [
@@ -16,6 +21,7 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
        { path: 'products/:productId', component: ProductDetailsComponent },
+      { path: 'resume', component: MyResumeComponent },
     ])
   ],
   declarations: [
@@ -23,7 +29,12 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    MyResumeComponent,
+    ResumeEducationComponent,
+    ResumeFooterComponent,
+    ResumeProfileComponent,
+    ResumeWorkExperienceComponent
   ],
   bootstrap: [ AppComponent ]
 })
